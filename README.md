6a59ea885c08d85d36cb97b328246f4880e50def
mathe42 <2pi_r2@gmx.de>
2022-09-24 18:29:30 +0200

    inital commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# surrealql README
+
+## Features
+
+### Syntax higlighting (WIP)
+In `.surql` files and in TS/JS-Files useing taged-template-litarals you get syntax highlihting!
+
+For example
+```ts
+const query = surql`
+  SELECT * FROM t
+`
+```
+will have highlighting!
+
+> Help wanted! If you want to help look into `./syntaxes/surrealql.json`!
+
+### Language config
+See `./language-configuration.json`
+
+### Language server (WIP)
+Integration with language server!
---
+++ src/extension.ts
@@ -0,0 +1,28 @@
+import * as vscode from 'vscode';
+import { workspace, window, ExtensionContext, TextDocument, OutputChannel, WorkspaceFolder, Uri } from "vscode";
+import { LanguageClient, LanguageClientOptions, TransportKind } from "vscode-languageclient/node";
+
+export function activate(context: ExtensionContext) {
+	const pathToLangServer = '/home/sebastian/surrealdb/targets/debug/surrealdb'
+	const serverArguments = ['lsp']
+
+	const server = {
+		command: pathToLangServer,
+		args: serverArguments
+	}
+
+	const client = new LanguageClient('surrealdb1', {
+		run: server,
+		debug: server
+		}, {
+			documentSelector: [
+				{scheme: 'file', language: 'surrealql'}
+			]
+		}
+	)
+	
+	client.start()
+}
+
+
+export function deactivate() {}
